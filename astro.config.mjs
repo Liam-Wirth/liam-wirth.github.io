531ac8feaa637a9476dd704f4347430c1f6ba33c
Liam <ltwirth@asu.edu>
2024-07-21 10:44:44 -0700

    formatting, and fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://wirthless.dev",
+  site: 'https://wirthless.dev',
   server: {
     port: 3000,
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
 });
