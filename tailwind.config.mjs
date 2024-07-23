3b5c6173f92a9a5c10a83c49cc7791c6cb59cde1
Liam <ltwirth@asu.edu>
2024-07-23 16:19:51 -0700

    MINTgit add .! FOOOKIN MINT

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,35 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 //TODO: Fix this, colors aren't working consistently between this file and the global.css file
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}'],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         gray: {
-          0: '#090b11',
-          50: '#141925',
-          100: '#283044',
-          200: '#3d4663',
-          300: '#505d84',
-          400: '#6474a2',
-          500: '#8490b5',
-          600: '#a3acc8',
-          700: '#c3cadb',
-          800: '#e3e6ee',
-          900: '#f3f4f7',
-          999: '#ffffff',
-          '999_basis': '0, 0%, 100%',
-          999_40: 'hsla(var(--gray-999-basis), 0.4)',
+          0: "var(--gray-0)",
+          50: "var(--gray-50)",
+          100: "var(--gray-100)",
+          200: "var(--gray-200)",
+          300: "var(--gray-300)",
+          400: "var(--gray-400)",
+          500: "var(--gray-500)",
+          600: "var(--gray-600)",
+          700: "var(--gray-700)",
+          800: "var(--gray-800)",
+          900: "var(--gray-900)",
+          999: "var(--gray-999)",
+          '999_basis': "var(--gray-999-basis)",
+          999_40: "var(--gray-999_40)",
+          999: "var(--gray-999)",
+
         },
         accent: {
-          light: '#c561f6',
-          regular: '#7611a6',
-          dark: '#1c0056',
-          overlay: 'hsla(280, 89%, 67%, 0.33)',
-          subtle_overlay: 'hsla(281, 81%, 36%, 0.33)',
-          text_over: '#ffffff',
+          light: "var(--accent-light)",
+          regular: "var(--accent-regular)",
+          dark: "var(--accent-dark)",
+          overlay:"var(--accent-overlay)",
+          subtle_overlay: "var(--accent-subtle-overlay)",
+          text_over: "var(--accent-text-over)", 
         },
-        link: '#7611a6',
+        link: "var(--accent-regular)", 
+        gradient: {
+          stop_1: "var(--gradient-stop-1)",
+          stop_2: "var(--gradient-stop-2)",
+          stop_3: "var(--gradient-stop-3)",
+          subtle: "var(--gradient-subtle)",
+          accent: "var(--gradient-accent)",
+          accent_orange: "var(--gradient-accent-orange)",
+          stroke: "var(--gradient-stroke)",
+        },
+        shadow: {
+
+        }
       },
       backgroundImage: {
         'gradient-subtle':
@@ -42,16 +57,6 @@ export default {
           'linear-gradient(180deg, var(--gray-900), var(--gray-700))',
       },
 
-      gradientColorStops: {
-        'gradient-stop-1': '#c561f6',
-        'gradient-stop-2': '#7611a6',
-        'gradient-stop-3': '#1c0056',
-      },
-      boxShadow: {
-        sm: '0px 6px 3px rgba(9, 11, 17, 0.01), 0px 4px 2px rgba(9, 11, 17, 0.01), 0px 2px 2px rgba(9, 11, 17, 0.02), 0px 0px 1px rgba(9, 11, 17, 0.03)',
-        md: '0px 28px 11px rgba(9, 11, 17, 0.01), 0px 16px 10px rgba(9, 11, 17, 0.03), 0px 7px 7px rgba(9, 11, 17, 0.05), 0px 2px 4px rgba(9, 11, 17, 0.06)',
-        lg: '0px 62px 25px rgba(9, 11, 17, 0.01), 0px 35px 21px rgba(9, 11, 17, 0.05), 0px 16px 16px rgba(9, 11, 17, 0.1), 0px 4px 9px rgba(9, 11, 17, 0.12)',
-      },
       fontSize: {
         sm: '0.875rem',
         base: '1rem',
