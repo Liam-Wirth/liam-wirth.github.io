3371fd863fc43a976b12274c779dcf048aa30f53
Liam <ltwirth@asu.edu>
2024-09-29 11:25:21 -0700

    possible svelte move

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wirthless.dev',
   server: {
-    port: 3000,
+    port: 3000
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()]
 });
---
+++ src/components/icons/_icon.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let size = "1em";
+  export let color = "currentColor";
+  export let pathData = ""; // This will be overridden by specific icons
+</script>
+
+<svg
+  width={size}
+  height={size}
+  fill={color}
+  xmlns="http://www.w3.org/2000/svg"
+  {...$$restProps}
+>
+  <path d={pathData} />
+</svg>
